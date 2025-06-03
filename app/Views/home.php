02b8ff26cb9990774dabc51a737a2e2add1352ef
yimmy20 <yimicrisma@gmail.com>
2025-06-03 17:09:35 -0500

    test commit

--- app/Views/home.php
+++ app/Views/home.php
@@ -8,6 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
     <style>
         body {
+            background-color: white;
             min-height: 100vh;
             display: flex;
             flex-direction: column;
